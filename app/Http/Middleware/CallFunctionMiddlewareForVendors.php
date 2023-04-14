191946b6ba05f1ffc5a450fe72d380e913e12fef
Hannington Kizza <hanningtonkizza@gmail.com>
2023-04-14 15:27:06 +0300

    middleware

---
+++ app/Http/Middleware/CallFunctionMiddlewareForVendors.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CallFunctionMiddlewareForVendors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $vendor = DB::select("select * from vendor where vendorId = ?", [session('vendor')[0]->vendorId]);
+        session(['vendor'=> $vendor]);
+        return $next($request);
+    }
+}
